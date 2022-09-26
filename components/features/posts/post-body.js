e211109d218763655d187d91015f66269a157d31
tienpm98 <phaminhtien2498@gmail.com>
2022-09-27 00:51:10 +0700

    taiwind styles

--- components/features/posts/post-body.js
+++ components/features/posts/post-body.js
@@ -30,7 +30,7 @@ export default function PostBody({ content }) {
 						<Chip key={index} title={tag} />
 					))}
 				</div>
-				<div className='pt-20 breadcrumb_text'>
+				<div className='pt-20'>
 					<Breadcrumb />
 				</div>
 			</div>
--- components/layout/breadcrumb/breadcrumb.js
+++ components/layout/breadcrumb/breadcrumb.js
@@ -2,14 +2,22 @@ import Breadcrumbs from 'nextjs-breadcrumbs'
 
 const Breadcrumb = () => {
 	return (
-		<>
+		<div
+			className='
+			[&>nav>ol]:flex [&>nav>ol]:items-center [&>nav>ol]:list-none [&>nav>ol]:p-0
+			[&>nav>ol>li]:p-0 [&>nav>ol>li]:m-0 [&>nav>ol>li]:mr-4
+			[&>nav>ol>li:after]:content-["›"] [&>nav>ol>li:after]:inline-block [&>nav>ol>li:after]:ml-4
+			[&>nav>ol>li:last-child:after]:content-[""]
+			[&>nav>ol>li>a]:text-12 [&>nav>ol>li>a]:no-underline [&>nav>ol>li>a]:text-black [&>nav>ol>li>a]:font-displayNormal [&>nav>ol>li>a]:font-bold
+		'
+		>
 			<Breadcrumbs
 				useDefaultStyle
 				rootLabel='HOME'
 				transformLabel={(title) => title.toUpperCase()}
 				omitIndexList={[1]}
 			/>
-		</>
+		</div>
 	)
 }
 
