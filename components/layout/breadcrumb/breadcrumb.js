a0ce8db993615b001f227333a63af4f89067a097
tienpm98 <phaminhtien2498@gmail.com>
2022-09-27 11:01:21 +0700

    test

--- components/layout/breadcrumb/breadcrumb.js
+++ components/layout/breadcrumb/breadcrumb.js
@@ -2,16 +2,7 @@ import Breadcrumbs from 'nextjs-breadcrumbs'
 
 const Breadcrumb = () => {
 	return (
-		<div
-			className='
-			not-prose
-			list_breadcrumb:flex list_breadcrumb:items-center list_breadcrumb:list-none list_breadcrumb:p-0
-			breadcrumb_items:p-0 breadcrumb_items:m-0 breadcrumb_items:mr-4
-			breadcrumb_next:content-["›"] breadcrumb_next:inline-block breadcrumb_next:ml-4
-			breadcrumb_items_last:after:content-[""]
-			breadcrumb_link:text-12 breadcrumb_link:no-underline breadcrumb_link:text-black breadcrumb_link:font-displayNormal breadcrumb_link:font-bold
-		'
-		>
+		<div className='next_breadcrumb'>
 			<Breadcrumbs
 				useDefaultStyle
 				rootLabel='HOME'
