11178b8caf2520827c154e97897378122da865fe
tienpm98 <phaminhtien2498@gmail.com>
2022-09-27 11:18:38 +0700

    fix

--- components/layout/breadcrumb/breadcrumb.js
+++ components/layout/breadcrumb/breadcrumb.js
@@ -2,14 +2,14 @@ import Breadcrumbs from 'nextjs-breadcrumbs'
 
 const Breadcrumb = () => {
 	return (
-		<div className='next_breadcrumb'>
+		<>
 			<Breadcrumbs
 				rootLabel='HOME'
 				transformLabel={(title) => title.toUpperCase()}
 				omitIndexList={[1]}
 				listClassName='list_breadcrumb'
 			/>
-		</div>
+		</>
 	)
 }
 
